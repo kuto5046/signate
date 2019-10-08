595e321a8c12c0a51052ed9b66269892952420f2
kyohei <h27.kubb@gmail.com>
2019-10-08 19:18:02 +0900

    lgb model

--- script/baseline.py
+++ script/baseline.py
@@ -7,6 +7,7 @@ import requests
 import time
 from bs4 import BeautifulSoup
 from tqdm import tqdm
+import datetime
 import matplotlib.pyplot as plt
 import lightgbm as lgb
 from sklearn.metrics import mean_squared_error, roc_auc_score
@@ -17,7 +18,7 @@ from sklearn.model_selection import train_test_split
 # from geopy.geocoders import Nominatim
 # from geopy.extra.rate_limiter import RateLimiter
 
-
+date = datetime.datetime.today().strftime("%Y%m%d_%H%M%S")
 train = pd.read_csv("../input/train.csv")
 test = pd.read_csv("../input/test.csv")
 
@@ -38,20 +39,58 @@ def feature_arrange(df, type):
     # 所在地
     place = df["Place"].str.replace("東京都", "").str.split("区")
     place = pd.DataFrame(place.str, index=["Place1", "Place2"]).T
-    place = pd.get_dummies(place["Place1"])
-    place = place.rename(columns={'世田谷': 'Setagaya', '中央': 'Cyuo', '中野': 'Nakano',
-                                  '北': 'Kita', '千代田': 'Tiyoda', '台東': 'Daito',
-                                  '品川': 'Sinagawa', '墨田': 'Sumida', '大田': 'Ota',
-                                  '文京': 'Bunkyo', '新宿': 'Sinzyuku', '杉並': 'Suginami',
-                                  '板橋': 'Itabashi', '江戸川': 'Edogawa', '江東': 'Eto',
-                                  '渋谷': 'Shibuya', '港': 'Minato', '目黒': 'Meguro', '練馬': 'Nerima',
-                                  '荒川': 'Arakawa', '葛飾': 'Katushika', '豊島': 'Toyoshima',
-                                  '足立': 'Adati'})
+    place = place["Place1"].str.replace("港", "0")
+    place = place.str.replace("千代田", "0")
+    place = place.str.replace("中央", "0")
+    place = place.str.replace("渋谷", "0")
+    place = place.str.replace("目黒", "0")
+
+    place = place.str.replace("新宿", "1")
+    place = place.str.replace("文京", "1")
+    place = place.str.replace("台東", "1")
+    place = place.str.replace("江東", "1")
+    place = place.str.replace("品川", "1")
+
+    place = place.str.replace("荒川", "2")
+    place = place.str.replace("墨田", "2")
+    place = place.str.replace("世田谷", "2")
+    place = place.str.replace("豊島", "2")
+    place = place.str.replace("大田", "2")
+
+    place = place.str.replace("中野", "3")
+    place = place.str.replace("北", "3")
+    place = place.str.replace("杉並", "3")
+    place = place.str.replace("練馬", "3")
+    place = place.str.replace("板橋", "3")
+
+    place = place.str.replace("江戸川", "4")
+    place = place.str.replace("足立", "4")
+    place = place.str.replace("葛飾", "4")
+    place = pd.DataFrame(place).astype(int)
+    # place = pd.get_dummies(place["Place1"])
+    # place = place.rename(columns={'世田谷': 'Setagaya', '中央': 'Cyuo', '中野': 'Nakano',
+    #                               '北': 'Kita', '千代田': 'Tiyoda', '台東': 'Daito',
+    #                               '品川': 'Sinagawa', '墨田': 'Sumida', '大田': 'Ota',
+    #                               '文京': 'Bunkyo', '新宿': 'Sinzyuku', '杉並': 'Suginami',
+    #                               '板橋': 'Itabashi', '江戸川': 'Edogawa', '江東': 'Eto',
+    #                               '渋谷': 'Shibuya', '港': 'Minato', '目黒': 'Meguro', '練馬': 'Nerima',
+    #                               '荒川': 'Arakawa', '葛飾': 'Katushika', '豊島': 'Toyoshima',
+    #                               '足立': 'Adati'})
     # 間取り、方角, 建物構造
     # room = pd.get_dummies(df["Room"].str.replace("納戸", ""))
-    angle = pd.get_dummies(df["Angle"])
-    angle = angle.rename(columns={"北": "N", "北東": "NE", "北西": "NW", "南": "S",
-                                  "南東": "SE", "南西": "SW", "東": "E", "西": "W" })
+    # angle = pd.get_dummies(df["Angle"])
+    # angle = angle.rename(columns={"北": "N", "北東": "NE", "北西": "NW", "南": "S",
+    #                               "南東": "SE", "南西": "SW", "東": "E", "西": "W"})
+    angle = df["Angle"].str.replace("北西", "1")
+    angle = angle.str.replace("北東", "1")
+    angle = angle.str.replace("南西", "3")
+    angle = angle.str.replace("南東", "3")
+    angle = angle.str.replace("北", "0")
+    angle = angle.str.replace("西", "2")
+    angle = angle.str.replace("東", "2")
+    angle = angle.str.replace("南", "4")
+    angle = pd.DataFrame(angle.fillna("2"), columns=["Angle"]).astype(int)
+    
     material = pd.get_dummies(df["Material"].str.replace("鉄筋ブロック", "ブロック"))
     material = material.rename(columns={'ALC（軽量気泡コンクリート）': 'ALC', 'HPC（プレキャスト・コンクリート（重量鉄骨））': 'HPC',
                                         'PC（プレキャスト・コンクリート（鉄筋コンクリート））': 'PC', 'RC（鉄筋コンクリート）': 'RC',
@@ -59,7 +98,7 @@ def feature_arrange(df, type):
                                         '木造': 'Wood', '軽量鉄骨': "LSteel", '鉄骨造': 'Steel'})
     # 築年数の前処理
     passed = df["Passed"].str.replace("新築", "0年").str.split("年", expand=True)
-    passed = pd.DataFrame(passed[0]).astype(int)
+    passed = pd.DataFrame(passed[0], columns=["Passed"]).astype(int)
 
     # 面積
     area = df["Area"].str.replace('m2', '').astype(float).astype(int)
@@ -71,11 +110,19 @@ def feature_arrange(df, type):
     floor["MaxFloor"] = floor['MaxFloor'].fillna("4")  # テストデータの欠損値を埋める用
     max_floor = floor["MaxFloor"].astype(int)
 
+    # 浴槽とか
+    bath = pd.DataFrame(df["Bath"].str.split("／", expand=True).nunique(axis=1), columns=["Bath"])
+    kitchen = pd.DataFrame(df["Kitchen"].str.split("／", expand=True).nunique(axis=1), columns=["Kitchen"])
+    facility = pd.DataFrame(df["Facility"].str.split("／", expand=True).nunique(axis=1), columns=["Facility"])
+    internet = pd.DataFrame(df["Internet"].str.split("／", expand=True).nunique(axis=1), columns=["Internet"])
+
     if type == "train":
         target = df['target']
-        new_df = pd.concat([target, place, passed, angle, area, max_floor, material], axis=1)
+        new_df = pd.concat([target, place, passed, angle, area, max_floor, material,
+                            bath, kitchen, facility, internet], axis=1)
     else:
-        new_df = pd.concat([place, passed, angle, area, max_floor, material], axis=1)
+        new_df = pd.concat([place, passed, angle, area, max_floor, material,
+                            bath, kitchen, facility, internet], axis=1)
     return new_df
 
 
@@ -90,7 +137,7 @@ y_pred = np.zeros(X_test.shape[0], dtype='float32')
 train_pred = np.zeros(X.shape[0], dtype='float32')
 
 cv_score = 0
-X_train, X_valid, y_train, y_valid = train_test_split(X, y, random_state=0)
+X_train, X_valid, y_train, y_valid = train_test_split(X, y, shuffle=True, random_state=0)
 train_data = lgb.Dataset(X_train, y_train)
 valid_data = lgb.Dataset(X_valid, y_valid)
 
@@ -122,16 +169,14 @@ y_pred += model.predict(X_test, num_iteration=model.best_iteration)
 cv_score += val_score
 print(cv_score)
 
-
 feature_importances = pd.DataFrame()
 feature_importances['feature'] = X.columns
 feature_importances['importance'] = model.feature_importance()
 feature_importances = feature_importances.sort_values(by='importance', ascending=False)
 plt.figure(figsize=(16, 16))
 sns.barplot(data=feature_importances.head(50), x='importance', y='feature')
+plt.show()
 
 submit = pd.read_csv('../input/sample_submit.csv', names=('id', 'target'))
 submit['target'] = y_pred
-submit.head(10)
-
-submit.to_csv('submit_kernel_01.csv', header=False, index=False)
+submit.to_csv('../output/submit{}.csv'.format(date), header=False, index=False)
